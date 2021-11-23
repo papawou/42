613a5c9baecd2dd356b1193ad75c35b36b2f13de
Mendes Kevin <papawa.dev@gmail.com>
2021-11-23 16:38:55 +0100

    swap func poly

--- push_swap/src/stack.c
+++ push_swap/src/stack.c
@@ -38,8 +38,85 @@ void stack_rpush(t_stack *stack, t_stack_elem *elem) //push to tail
 	stack->tail = elem;
 }
 
-void swap(t_stack *stack, t_stack_elem *elem1, t_stack_elem *elem2)
+t_stack_elem *stack_remove(t_stack_elem *A)
 {
-	elem1 = elem2;
-	stack->head = elem1;
+	if (A->prev)
+		A->prev->next = A->next;
+	else
+		A->stack->tail = A->next;
+	if (A->next)
+		A->next->prev = A->prev;
+	else
+		A->stack->head = A->prev;
+	A->prev = NULL;
+	A->next = NULL;
+	A->stack = NULL;
+	return (A);
+}
+
+// A -next> <prev- B
+void swap_adj(t_stack_elem *A, t_stack_elem *B)
+{
+	B->prev = A->prev;
+	A->next = B->next;
+
+	B->next = A;
+	A->prev = B;
+}
+
+//AB -next> ... <prev- AB
+void swap_dist(t_stack_elem *A, t_stack_elem *B)
+{
+	t_stack_elem *tmp;
+
+	tmp = A->prev;
+	A->prev = B->prev;
+	B->prev = tmp;
+
+	tmp = A->next;
+	A->next = B->next;
+	B->next = tmp;
+}
+
+// [ AB ] [ AB ]
+void sswap(t_stack_elem *A, t_stack_elem *B)
+{
+	void *tmp;
+
+	tmp = A->next;
+	A->next = B->next;
+	B->next = tmp;
+	tmp = A->prev;
+	A->prev = B->prev;
+	B->prev = tmp;
+	tmp = A->stack;
+	A->stack = B->stack;
+	B->stack = tmp;
+}
+
+void swap(t_stack_elem *A, t_stack_elem *B)
+{
+	if (!A || !B)
+		return ;
+	if (A->stack != B->stack) //[ AB ] [ AB ]
+		sswap(A, B);
+	else if (A->next == B) // A-next> B
+		swap_adj(A, B);
+	else if (A->prev == B) // B -next> A
+		swap_adj(B, A);
+	else
+		swap_dist(A, B); // AB -> ... AB
+	if (!A->prev)
+		A->stack->tail = A;
+	if (!A->next)
+		A->stack->head = A;
+	if (!B->prev)
+		B->stack->tail = B;
+	if (!B->next)
+		B->stack->head = B;
+}
+
+
+void move_s(t_stack *stack) //move two top
+{
 }
